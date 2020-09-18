ce623dc8339351beedec416c8e912981346d2ef2
Alfonso <alfof87@gmail.com>
2020-09-18 16:30:02 +0200

    laravel-boolean

--- laravel-boolean/resources/views/layouts/main-layout.blade.php
+++ laravel-boolean/resources/views/layouts/main-layout.blade.php
@@ -2,11 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>i'm the main layout</title>
+    <link rel="stylesheet" href="{{ asset("/css/app.css") }}">
   </head>
   <body>
     <header>
-      @include("partials.header");
+      @include("layouts.partials.header")
+      @include("layouts.partials.main")
+      @include("layouts.partials.footer")
     </header>
   </body>
 </html>
--- laravel-boolean/resources/views/layouts/partials/header.blade.php
+++ laravel-boolean/resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,12 @@
-<div class="">
-  {{-- <img src="" alt=""> --}}
-  @php($data)
-    {{ $data }}
-  @endphp
+<div class="container">
+  <img src="{{ asset('images/marchio-sito-test.png') }}" alt="">
 </div>
+<div class="container-home">
+  <h2><a href="#">Home</a></h2>
+  <h2><a href="#">Prodotti</a></h2>
+  <h2><a href="#">News</a></h2>
+</div>
+
+{{-- @php($data)
+{{ $data }}
+@endphp --}}
